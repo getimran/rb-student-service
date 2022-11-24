a43bfb1858d471ff88e29f5e8253c8d45977a5f9
Imran <>
2022-11-24 17:44:54 +0530

    project structure initial commit

---
+++ src/main/java/ae/rakbank/student/RbStudentServiceApplication.java
@@ -0,0 +1,13 @@
+package ae.rakbank.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbStudentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbStudentServiceApplication.class, args);
+	}
+
+}
